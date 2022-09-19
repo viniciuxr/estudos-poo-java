de40ff225ce344f9c6944a75acc348923b44680b
viniciuxr <mourav61@gmail.com>
2022-09-19 18:58:49 -0300

    Update Principal.java
    novas linhas

--- ProjetoInicial/src/empresa/Principal.java
+++ ProjetoInicial/src/empresa/Principal.java
@@ -21,6 +21,26 @@ public class Principal {
 		System.out.println("a idade é: "+idade); // + concatena
 		System.out.println("O peso é: "+peso);
 		
+		if (idade <18) {
+			System.out.println("Acesso negado, você é muito novo");
+		}
+		else if (idade <65) {	
+			System.out.println("Acesso, concedido, você é um adulto");
+		}	
+		
+		else {
+			System.out.println("Acesso concedido, tá velhinho hein");
+		}
+		
+		for(int i=0; i<10; i++) { //++ significa +=1
+			System.out.println("Valor: "+i);
+		}
+		//array
+		//lista
+		int megaSena[] = {11,14,21,30,36,34,56};
+		int numeros[] = new int[200];
+		numeros[60] = 50;
+		megaSena[0] = 10; //modificando valor
 	}
 
 }
