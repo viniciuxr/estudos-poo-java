cd2043fd9603e84b03688ac82d7ba12884365e34
viniciuxr <mourav61@gmail.com>
2022-09-20 16:50:49 -0300

    Create Aula1Pt2.java
    teste de for e arrays

---
+++ Aula1Pt2/src/aula1pt2/Aula1Pt2.java
@@ -0,0 +1,18 @@
+package aula1pt2;
+
+public class Aula1Pt2 {
+
+	public static void main(String[] args) {
+		for(int i=0; i<10; i++) { //++ significa +=1
+			System.out.println("Valor: "+i);
+		}
+		//array
+		//lista
+		int megaSena[] = {11,14,21,30,36,34,56};
+		int numeros[] = new int[200];
+		numeros[60] = 50;
+		megaSena[0] = 10; //modificando valor
+
+	}
+
+}
