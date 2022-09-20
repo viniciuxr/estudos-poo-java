3411a96053dca47a35069e711ed3a35063e3a590
viniciuxr <mourav61@gmail.com>
2022-09-20 15:53:40 -0300

    Create IMC.java
    programa básico de imc em java

---
+++ IMC/src/imc/IMC.java
@@ -0,0 +1,18 @@
+package imc;
+
+import java.util.Scanner;
+
+public class IMC {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Digite seu peso e sua altura, respectivamente");
+		Scanner input = new Scanner(System.in);
+		int peso = input.nextInt();
+		double altura = input.nextDouble();
+		
+
+	}
+
+}
