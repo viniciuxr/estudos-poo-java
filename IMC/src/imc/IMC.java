7bdf8d1379dc36337910c2d857aa7ad0f7d874ae
viniciuxr <mourav61@gmail.com>
2022-09-20 16:46:48 -0300

    Update IMC.java
    calculadora de imc

--- IMC/src/imc/IMC.java
+++ IMC/src/imc/IMC.java
@@ -6,11 +6,15 @@ public class IMC {
 
 	public static void main(String[] args) {
 		
+		int peso;
+		double altura;
 		
-		System.out.println("Digite seu peso e sua altura, respectivamente");
+		System.out.println("Digite abaixo seu peso e sua altura, respectivamente: ");
 		Scanner input = new Scanner(System.in);
-		int peso = input.nextInt();
-		double altura = input.nextDouble();
+		peso = input.nextInt();
+		altura = input.nextDouble();
+		double imc = peso/(altura*altura);
+		System.out.printf("O seu IMC é: %.2f",imc); //print f e %.2f para cortar as casas decimais, poderia colocar %d para n° inteiros ou %s para strings
 		
 
 	}
