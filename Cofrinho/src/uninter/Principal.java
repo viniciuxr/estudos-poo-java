7ffe92281290dee3479f83dced9c340172a280f5
viniciuxr <mourav61@gmail.com>
2022-10-07 23:22:57 -0300

    Create Principal.java

---
+++ Cofrinho/src/uninter/Principal.java
@@ -0,0 +1,12 @@
+package uninter;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		//Menu menu = new Menu();
+		//menu.exibirMenuPrincipal();
+		Real moedaDeUmReal = new Real();
+		moedaDeUmReal.info();
+	}
+
+}
