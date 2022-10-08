02112ea3f5742a0a023483d1c507dd9d1c043b98
viniciuxr <mourav61@gmail.com>
2022-10-08 00:16:49 -0300

    Update Principal.java

--- Cofrinho/src/uninter/Principal.java
+++ Cofrinho/src/uninter/Principal.java
@@ -1,19 +1,42 @@
 package uninter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Principal {
 
 	public static void main(String[] args) {
 		//Menu menu = new Menu();
 		//menu.exibirMenuPrincipal();
-		Moeda moedaDeUmReal = new Real(1);
+		Real moedaDeUmReal = new Real(1);
 		moedaDeUmReal.info();
 		System.out.println(moedaDeUmReal.converter());
 		
+		Real outraMoedaDeReal = new Real(5);
+		moedaDeUmReal.info();
+		System.out.println(moedaDeUmReal.converter());
+		
+		
+		
 		System.out.println("------------------------");
 		
-		Moeda moedaDeDolar = new Dolar(5);
+		Dolar moedaDeDolar = new Dolar(5);
 		moedaDeDolar.info();
 		System.out.println(moedaDeDolar.converter());
+		
+		System.out.println("------------------------");
+		
+		ArrayList<Moeda> listaDeMoedas = new ArrayList<>();
+		System.out.println("Lista de moedas: "+listaDeMoedas.toString());
+		
+		listaDeMoedas.add(moedaDeUmReal);
+		listaDeMoedas.add(outraMoedaDeReal);
+		listaDeMoedas.add(moedaDeDolar);
+		
+		for(Moeda moedaDeReal: listaDeMoedas) {
+			moedaDeReal.info();
+		}
 	}
+
 
 }
