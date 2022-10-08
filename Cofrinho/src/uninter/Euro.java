70bbdf0170c036033a562c1fa34ae5e8cd0bc9b1
viniciuxr <mourav61@gmail.com>
2022-10-08 01:03:58 -0300

    Create Euro.java

---
+++ Cofrinho/src/uninter/Euro.java
@@ -0,0 +1,20 @@
+package uninter;
+
+public class Euro extends Moeda {
+
+	public Euro(double valorInicial) {
+		this.valor = valorInicial;
+	}
+	
+	public void info() {
+		System.out.println("Euro - "+valor);
+
+		
+	}
+
+
+	public double converter() {
+		return this.valor*5.07;
+	}
+
+}
