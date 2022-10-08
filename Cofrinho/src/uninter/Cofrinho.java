4b4b0af68c336e6182e686c5bb30c64527877876
viniciuxr <mourav61@gmail.com>
2022-10-08 01:03:54 -0300

    Update Cofrinho.java

--- Cofrinho/src/uninter/Cofrinho.java
+++ Cofrinho/src/uninter/Cofrinho.java
@@ -1,9 +1,19 @@
 package uninter;
 
+import java.util.ArrayList;
+
 public class Cofrinho {
+	private ArrayList<Moeda> listaMoedas;
 
-
-
+	public Cofrinho() {
+		this.listaMoedas = new ArrayList<>();
+		
 	}
+	
+	public void adicionar(Moeda moeda) {
+		this.listaMoedas.add(moeda);
+	}
+
+}
 
 
