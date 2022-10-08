867fee4c1cc0697231f61b4470b43953b54d0a36
viniciuxr <mourav61@gmail.com>
2022-10-07 23:51:55 -0300

    Update Principal.java

--- Cofrinho/src/uninter/Principal.java
+++ Cofrinho/src/uninter/Principal.java
@@ -5,8 +5,15 @@ public class Principal {
 	public static void main(String[] args) {
 		//Menu menu = new Menu();
 		//menu.exibirMenuPrincipal();
-		Real moedaDeUmReal = new Real();
+		Moeda moedaDeUmReal = new Real(1);
 		moedaDeUmReal.info();
+		System.out.println(moedaDeUmReal.converter());
+		
+		System.out.println("------------------------");
+		
+		Moeda moedaDeDolar = new Dolar(5);
+		moedaDeDolar.info();
+		System.out.println(moedaDeDolar.converter());
 	}
 
 }
