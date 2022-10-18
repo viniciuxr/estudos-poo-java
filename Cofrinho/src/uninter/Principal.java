a2a18968a4ffe9d0cefec5c224f39ed44123f191
Paulo Vinícius <110192237+viniciuxr@users.noreply.github.com>
2022-10-17 22:10:04 -0300

    Update Principal.java

--- Cofrinho/src/uninter/Principal.java
+++ Cofrinho/src/uninter/Principal.java
@@ -1,28 +1,14 @@
 package uninter;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class Principal {
 
 	public static void main(String[] args) {
-		//Menu menu = new Menu();
-		//menu.exibirMenuPrincipal();
+		Menu menu = new Menu();
+		menu.exibirMenuPrincipal();
 		
-		Cofrinho cofrinho = new Cofrinho();
-		cofrinho.listagemMoedas();
 		
-		Real doisReais = new Real(2);
-		Dolar cincoDolar = new Dolar(5);
-		Euro vinteECincoEuros = new Euro(25);
-		Real tresReais = new Real(3);
-		
-		cofrinho.adicionar(doisReais);
-		cofrinho.adicionar(cincoDolar);
-		cofrinho.adicionar(vinteECincoEuros);
-		cofrinho.adicionar(tresReais);
-		
-		cofrinho.listagemMoedas();
 		}
 }
 
