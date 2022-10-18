3e130ec6bcd9c41ceebc340c743423c30bde1111
Paulo Vinícius <110192237+viniciuxr@users.noreply.github.com>
2022-10-17 22:10:09 -0300

    Update Menu.java

--- Cofrinho/src/uninter/Menu.java
+++ Cofrinho/src/uninter/Menu.java
@@ -5,9 +5,11 @@ import java.util.Scanner;
 public class Menu {
 	
 	private Scanner sc;
+	private Cofrinho cofrinho;
 	
 	public Menu() {
 		sc = new Scanner(System.in);
+		cofrinho = new Cofrinho();
 	}
 
 	public void exibirMenuPrincipal() {
@@ -24,11 +26,61 @@ public class Menu {
 		case"0":
 			System.out.println("Operação finalizada");
 			break;
+			
+		case"1":
+			exibirSubMenuAdicionarMoedas();
+			exibirMenuPrincipal();
+			
+			break;
+		case"3":
+			cofrinho.listagemMoedas();
+			exibirMenuPrincipal();
+			
+			break;
 		default:
 			System.out.println("Opção Inválida");
 			exibirMenuPrincipal();
 			break;
+			
+		case "4":
+			double valorTotalConvertido = cofrinho.totalConvertido();
+			System.out.println("Valor total convertido para real? "+valorTotalConvertido);
+			break;
 		}
 	}
+	private void exibirSubMenuAdicionarMoedas() {
+		System.out.println("Escolha a moeda");
+		System.out.println("1-Real");
+		System.out.println("2-Dolar");
+		System.out.println("3-Euro: ");
+		
+		int opcaoMoeda = sc.nextInt();
+		
+		System.out.println("Digite o valor: ");
+		String valorTextualMoeda = sc.next();
+		valorTextualMoeda = valorTextualMoeda.replace(",", ".");
+		
+		double valorMoeda = Double.valueOf(valorTextualMoeda);
+		
+		Moeda moeda=null;
+		
+		if (opcaoMoeda == 1) {
+			moeda = new Real (valorMoeda);
+			cofrinho.adicionar(moeda);
+		} else if(opcaoMoeda == 2) {
+			moeda = new Dolar (valorMoeda);
+			cofrinho.adicionar(moeda);
+		} else if(opcaoMoeda == 3 ) {
+			moeda = new Euro (valorMoeda);
+			cofrinho.adicionar(moeda);
+		}else {
+			System.out.println("Não existe essa moeda!");
+			exibirMenuPrincipal();
+		}
+		
 
+	
+		cofrinho.adicionar(moeda);
+		System.out.println("Moeda adicionada!");
+	}
 }
