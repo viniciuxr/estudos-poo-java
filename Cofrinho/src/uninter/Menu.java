f6fa51e808d40802b91f47d5bb00e65e47047eb1
Paulo Vinícius <110192237+viniciuxr@users.noreply.github.com>
2022-11-07 21:24:34 -0300

    Update Menu.java

--- Cofrinho/src/uninter/Menu.java
+++ Cofrinho/src/uninter/Menu.java
@@ -23,32 +23,33 @@ public class Menu {
 		String opcaoSelecionada = sc.next();
 		
 		switch (opcaoSelecionada) {
-		case"0":
-			System.out.println("Operação finalizada");
-			break;
-			
-		case"1":
-			exibirSubMenuAdicionarMoedas();
-			exibirMenuPrincipal();
-			
-			break;
-		case"3":
-			cofrinho.listagemMoedas();
-			exibirMenuPrincipal();
-			
-			break;
-		default:
-			System.out.println("Opção Inválida");
-			exibirMenuPrincipal();
-			break;
-			
-		case "4":
-			double valorTotalConvertido = cofrinho.totalConvertido();
-			System.out.println("Valor total convertido para real? "+valorTotalConvertido);
-			break;
+		
+			case"0":
+				System.out.println("Operação finalizada");
+				break;
+				
+			case"1":
+				exibirSubMenuAdicionarMoedas();
+				exibirMenuPrincipal();
+				
+				break;
+			case"3":
+				cofrinho.listagemMoedas();
+				exibirMenuPrincipal();
+				
+				break;
+			default:
+				System.out.println("Opção Inválida");
+				exibirMenuPrincipal();
+				break;
+				
+			case "4":
+				double valorTotalConvertido = cofrinho.totalConvertido();
+				System.out.println("Valor total convertido para real? "+valorTotalConvertido);
+				break;
 		}
 	}
-	private void exibirSubMenuAdicionarMoedas() {
+	 private void exibirSubMenuAdicionarMoedas() {
 		System.out.println("Escolha a moeda");
 		System.out.println("1-Real");
 		System.out.println("2-Dolar");
@@ -57,22 +58,22 @@ public class Menu {
 		int opcaoMoeda = sc.nextInt();
 		
 		System.out.println("Digite o valor: ");
-		String valorTextualMoeda = sc.next();
-		valorTextualMoeda = valorTextualMoeda.replace(",", ".");
 		
+		String valorTextualMoeda = sc.next();
+		
+		
+		valorTextualMoeda = valorTextualMoeda.replace(",", ".");
 		double valorMoeda = Double.valueOf(valorTextualMoeda);
+		
 		
 		Moeda moeda=null;
 		
 		if (opcaoMoeda == 1) {
 			moeda = new Real (valorMoeda);
-			cofrinho.adicionar(moeda);
 		} else if(opcaoMoeda == 2) {
 			moeda = new Dolar (valorMoeda);
-			cofrinho.adicionar(moeda);
 		} else if(opcaoMoeda == 3 ) {
 			moeda = new Euro (valorMoeda);
-			cofrinho.adicionar(moeda);
 		}else {
 			System.out.println("Não existe essa moeda!");
 			exibirMenuPrincipal();
@@ -82,5 +83,8 @@ public class Menu {
 	
 		cofrinho.adicionar(moeda);
 		System.out.println("Moeda adicionada!");
+		
+		
+		
 	}
 }
