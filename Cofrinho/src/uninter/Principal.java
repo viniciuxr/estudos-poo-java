5e724dad08581eb022fc126b7e3d3fddd2182a85
Paulo Vinícius <110192237+viniciuxr@users.noreply.github.com>
2022-11-07 22:50:31 -0300

    Update Principal.java

--- Cofrinho/src/uninter/Principal.java
+++ Cofrinho/src/uninter/Principal.java
@@ -5,16 +5,10 @@ import java.util.ArrayList;
 public class Principal {
 
 	public static void main(String[] args) {
-		//Menu menu = new Menu();
-		//menu.exibirMenuPrincipal();
+		Menu menu = new Menu();
+		menu.exibirMenuPrincipal();
 		
-		Real umReal = new Real(1);
 		
-		Real segundoUmReal = new Real(1);
-		
-		Real terceiroUmReal = new Real(1);
-		
-		System.out.println(umReal.equals(terceiroUmReal));
 	}
 }
 
