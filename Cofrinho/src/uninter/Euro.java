630f3a74bc08fb4099fc5c8377d1454d176f39dc
Paulo Vinícius <110192237+viniciuxr@users.noreply.github.com>
2022-11-07 22:50:35 -0300

    Update Euro.java

--- Cofrinho/src/uninter/Euro.java
+++ Cofrinho/src/uninter/Euro.java
@@ -16,5 +16,22 @@ public class Euro extends Moeda {
 	public double converter() {
 		return this.valor*5.07;
 	}
+	
+	@Override
+	public boolean equals(Object objeto) {
+		
+		if (this.getClass() != objeto.getClass()) {
+			return false;
+		}
+		
+		Euro objetoDeEuro = (Euro) objeto;
+		
+		if(this.valor != objetoDeEuro.valor) {
+			return false;
+		}
+		
+		return true;
+	}
+	
 
 }
