cfe6206d191be1133cfe1ea2aa717e99dd270cbb
Paulo Vinícius <110192237+viniciuxr@users.noreply.github.com>
2022-11-07 22:50:38 -0300

    Update Dolar.java

--- Cofrinho/src/uninter/Dolar.java
+++ Cofrinho/src/uninter/Dolar.java
@@ -16,5 +16,21 @@ public class Dolar extends Moeda {
 	public double converter() {
 		return this.valor*5.21;
 	}
+	
+	@Override
+	public boolean equals(Object objeto) {
+		
+		if (this.getClass() != objeto.getClass()) {
+			return false;
+		}
+		
+		Dolar objetoDeDolar = (Dolar) objeto;
+		
+		if(this.valor != objetoDeDolar.valor) {
+			return false;
+		}
+		
+		return true;
+	}
 
 }
